61f678b8dda3a7c057b574c6074b00a910d6cafd
remarkablerocket <remarkablerocket@users.noreply.github.com>
2015-04-18 22:31:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-install_reqs = [parse_requirements("requirements.txt", session=uuid.uuid1())
+install_reqs = parse_requirements("requirements.txt", session=uuid.uuid1())
 reqs = [str(req.req) for req in install_reqs]
 
 setup(
